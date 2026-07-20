e8af2f113d4adaf44f99fe9adce73c57a4c85e87
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-07-20 22:18:45 +0200

    Added navigation bar with name and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import InputText  from '@/Components/InputText';
+import bookMarkImage from '@/assets/bookmark.png'
 import type { ChangeEvent } from 'react';
 
 function App() {
   return (
     <>
-      
+        <div className='Nav'>
+            <div className='NavCard'>
+              <h2 className='Name'>Browser Links Vault</h2>
+            </div>
+
+            <div className='NavCard'>
+             <img src={bookMarkImage} alt='bookMark' />
+            </div>
+        </div>
+
+
     </>
   )
 }
