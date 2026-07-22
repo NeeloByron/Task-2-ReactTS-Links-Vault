be606427fc23f3110ccd4b9e1491b9499054f160
NeeloByron <byronnkhuna@gmail.com>
2026-07-22 09:07:05 +0200

    Deleted recordingmethod code for a different approach

--- src/Components/RecordingMethod.tsx
+++ src/Components/RecordingMethod.tsx
@@ -3,33 +3,7 @@
 export const RecordingMethod = () => {
   return (
    <>
-     <div className='recordOverly'>
-        <div className='recordContainer'>
-           
-           <div className='recordHeader'>
-            <h2 className='Model-title'>Add New</h2>
-            
-           </div>
-
-      <div className='RecordBody'>
-        <div className='formContainer'>
-            <label className='formLabel'>Title</label> 
-            <input type={'text'} className={'formInput'} placeholder={'Enter Title'} />
-
-            <label className='formLabel'>Link (URL)</label> 
-            <input type={'text'} className={'formInput'} placeholder={'Enter or paste your URL'} />
-
-            <label className='formLabel'>Description</label> 
-            <input type={'text'} className={'formInput'} placeholder={'Enter your description'} />
-
-            <label className='formLabel'>Optional Tags</label> 
-            <input type={'text'} className={'formInput'} placeholder={'Enter tags'} />
-        </div>
-      
-       </div>
-      </div>
      
-     </div>
    </>
   )
 }
