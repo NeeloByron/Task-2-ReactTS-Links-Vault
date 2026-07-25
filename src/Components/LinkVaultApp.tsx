859a9b376aaef1fdc7335204fe3327ece3317f25
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-07-25 18:29:53 +0200

    Restructured my header

--- src/Components/LinkVaultApp.tsx
+++ src/Components/LinkVaultApp.tsx
@@ -86,27 +86,14 @@ export const LinkVaultApp = () => {
   return (
          <>
            <div className={'main'}>
-
             <header className={'header'}>
-
-               <div className={'titleAndLogo'}>
-                 <img src={bookmarkIcon} alt='BookMarkicon' style={{ width: '67px', height: '60px' }} />
+               <div className={'title'}>
                  <h1>URL Archive</h1>
                </div>
-              
-              <div className={'buttonSide'}>
-                 <Button type='button' btnText='Add Link' style={{width: '11rem', 
-                  height: '3.8rem', 
-                  fontSize: '18px', backgroundColor: '#2196f3',
-                  borderRadius: '10px',
-                  border:'none',
-                  cursor:'pointer'}} onClick={() => { setEditingItem(null); setIsModalOpen(true);}} />
-              </div>
-
             </header>
-          
 
             <main className={'bodyContainer'}>
+              <img src={bookmarkIcon} alt='BookMarkicon' style={{ width: '67px', height: '60px' }} />
                <search className={'searchContainer'}>
                  <span className="Search-icon" >/</span>
                  <input type={'text'} className={'search-input'} 
@@ -120,6 +107,15 @@ export const LinkVaultApp = () => {
                    </button>
                    )}
                  </search>
+
+               <div className={'buttonSide'}>
+                 <Button type='button' btnText='Add Link' style={{width: '11rem', 
+                  height: '3.8rem', 
+                  fontSize: '18px', backgroundColor: '#2196f3',
+                  borderRadius: '10px',
+                  border:'none',
+                  cursor:'pointer'}} onClick={() => { setEditingItem(null); setIsModalOpen(true);}} />
+              </div>
                
               <section aria-label='Saved Links'>
                 <Table items={filteredItems} deleteItem={deleteItem} onEditClick={handleStartEdit} />
